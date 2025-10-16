287079585541d1d3308a768c6b2893b9a5854e85
Syed Ajmathulla <sdajmathulla@gmail.com>
2025-10-16 15:10:02 +0530

    for deploying neccessory files added

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://companydirectories.netlify.app/'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
